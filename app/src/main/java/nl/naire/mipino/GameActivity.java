939bdcb5d7fd9cb4330ce5fdc04d056026a31733
Joran Jessurun <joran.jessurun@gmail.com>
2019-04-21 12:21:16 +0200

    Made the basic layout.

--- app/src/main/java/nl/naire/mipino/GameActivity.java
+++ app/src/main/java/nl/naire/mipino/GameActivity.java
@@ -82,7 +82,7 @@ public class GameActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_clear_high_score) {
             return true;
         }
 
